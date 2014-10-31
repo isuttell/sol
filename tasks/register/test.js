3e332ce7ea40eb927164eb575505f9bf82db904e
Isaac Suttell <isaac@e-mc2.com>
2014-10-31 12:13:46 -0700

    npm test only tests backend js by default

--- tasks/register/test.js
+++ tasks/register/test.js
@@ -1,13 +1,12 @@
 module.exports = function (grunt) {
-    grunt.registerTask('test', [
-        'hint',
-        'jasmine_node:app'
-    ]);
-
     grunt.registerTask('test:js', [
         'copy:dev',
         'jshint',
         'jscs',
         'jasmine_node:app'
+    ]);
+
+    grunt.registerTask('test', [
+        'test:js'
     ]);
 };
